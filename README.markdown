15df9ec3213ae359de0502962139b0668b081a21
Mikulas <rullaf@gmail.com>
2013-01-13 11:33:50 +0100

    repo init

---
+++ README.markdown
@@ -0,0 +1,3 @@
+As per the requirements of Czech Post (http://www.ceskaposta.cz/cz/nastroje/informace-k-vyuziti-sluzby-track-and-trace--id34226/), usage of this library must not violate the following statement:
+
+> Without Czech Post's express consent in writing, any further dissemination, copying, storage, use, sale, distribution or reproduction of the [Track&trace] data is not permitted.
---
+++ Tracktrace.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Mikulas;
+
+require __DIR__ . '/vendor/autoload.php';
+
+
+class Tracktrace
+{
+
+	const LOCALE_CS = 'CZ';
+	const LOCALE_EN = 'CZ';
+
+	const URL = 'http://www.ceskaposta.cz/cz/nastroje/sledovani-zasilky.php?locale=%s&send.x=%d&send.y=%d&send=submit&go=ok&barcode=%s';
+	const PATTERN = '~^[A-Z]{2}[0-9]{9,10}[A-Z]{1,2}$~i';
+
+
+	public static function status($code, $locale = self::LOCALE_CS)
+	{
+		if (!self::validateCode($code)) {
+			throw new \InvalidArgumentException("Invalid code `$code` given, expected `" . self::PATTERN . "`.");
+		}
+		$url = sprintf(self::URL, $locale, rand(1, 30), rand(1, 30), $code);
+
+		$html = \Sunra\PhpSimple\HtmlDomParser::file_get_html($url);
+
+		$statuses = [];
+		foreach ($html->find('#content table td') as $status) {
+			$statuses[] = $string = html_entity_decode(trim($status->plaintext), ENT_QUOTES, "utf-8");;
+		}
+		return $statuses;
+	}
+
+
+
+	protected static function validateCode($code)
+	{
+		return preg_match(self::PATTERN, $code);
+	}
+
+}
